e42171f9caf03087d73596c09b820bdaa9226b34
Prince-Omotayo <quadriomotayo2024@gmail.com>
2023-03-16 23:25:30 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ *main - this is the main function tat contained the code
+ *
+ * Return: it return 0 always
+ **/
+int main(void){
+  puts("\"Programming is like building a multilingual puzzle");
+  return 0;
+}
