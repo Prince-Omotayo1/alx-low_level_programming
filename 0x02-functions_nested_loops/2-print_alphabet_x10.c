27b35843afd65e7623ceb5275d6c226a95ccc05c
Prince-Omotayo <quadriomotayo2024@gmail.com>
2023-03-22 19:23:34 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,17 @@
 #include "main.h"
 /**
  * print_alphabet_x10 - function that print 10x alphabets
- *
- * Return: always 0;
+ *                                                     * Return: always 0;
 */
 void print_alphabet_x10(void)
 {
-	int i;
-	char j;
+	int count = 0;
+	char letter;
 
-	for (i = 1; j <= 10; i++)
+	while (count++ <= 9)
 	{
-		for (j = 'a'; j <= '2'; j++)
-			_putchar(j);
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
 		_putchar('\n');
 	}
 }
